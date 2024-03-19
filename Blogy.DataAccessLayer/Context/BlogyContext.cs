de0e43e8a654d682c33ae239d8a941e9cbed3734
Nesibe93 <125722258+Nesibe93@users.noreply.github.com>
2024-03-19 14:23:00 +0300

    Add project files.

---
+++ Blogy.DataAccessLayer/Context/BlogyContext.cs
@@ -0,0 +1,25 @@
+﻿using Blogy.EntityLayer.Concreate;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogy.DataAccessLayer.Context
+{
+    public class BlogyContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) // Configürasyon
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;initial catalog=DbBlogy;integrated Security=true;");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
---
+++ Blogy.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogy.EntityLayer.Concreate
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string? CategoryName { get; set; } // Altında yeşil çizgi çıkınca Null olamaz demek
+        public List<Article> Articles { get; set; }
+
+    }
+}

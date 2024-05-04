61ffcf96e93de777515189a7ffd5582d8efdae65
Nesibe93 <125722258+Nesibe93@users.noreply.github.com>
2024-05-04 16:33:23 +0300

    son

---
+++ Blogy.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blogy.WebUI.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        [Area("Admin")]
+        [Route("Admin/Category")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
